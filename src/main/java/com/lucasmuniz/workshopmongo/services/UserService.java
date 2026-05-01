874678c070b81e5f26fe57fc840b4cf9b4bd6f2e
LucasMuni2005 <lucasmunizmiranda@gmail.com>
2026-05-01 10:58:19 -0300

    Obtendo um usuário por id

--- src/main/java/com/lucasmuniz/workshopmongo/services/UserService.java
+++ src/main/java/com/lucasmuniz/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.lucasmuniz.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lucasmuniz.workshopmongo.domain.User;
 import com.lucasmuniz.workshopmongo.reporsitory.UserRepository;
+import com.lucasmuniz.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,12 @@ public class UserService {
 	
 	public List<User> findAll(){
 		 return repo.findAll();
+		 
 	}
+	
+	public User findById(String id) { 
+		 Optional<User> obj = repo.findById(id); 
+		 return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")); 
+		}
 
 }
